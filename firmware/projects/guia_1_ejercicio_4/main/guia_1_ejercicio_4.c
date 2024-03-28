819b9d6347ce47580d2eabe9a3de59f347742585
MatiasMadrid <matias17madrid@gmail.com>
2024-03-28 15:45:22 -0300

    Se sube el proyecto 1 funcional, las actividades 5 y 6 están en el ejercicio 4

--- firmware/projects/guia_1_ejercicio_4/main/guia_1_ejercicio_4.c
+++ firmware/projects/guia_1_ejercicio_4/main/guia_1_ejercicio_4.c
@@ -38,6 +38,7 @@
 
 #define ON = 1
 #define OFF = 0
+#define N_DIGITOS = 3
 
 /*==================[internal functions declaration]=========================*/
 
@@ -65,34 +66,57 @@ void modificarStatus(uint32_t data, gpioConf_t *vectorGPIO){
 		}
 	}
 }
+void displayLeds(uint32_t data, uint digitos, gpioConf_t *vectorGPIO, gpioConf_t *vectorGPIO_map){
+	
+	uint8_t arreglo[digitos];
+	convertToBcdArray (data, digitos, arreglo);
+
+	for (int i=0; i<3; i++){ //define a digitos = 3
+		//on/off (pulso?)
+
+		modificarStatus(arreglo[i], vectorGPIO);
+
+		GPIOOn(vectorGPIO_map[i].pin);
+		GPIOOff(vectorGPIO_map[i].pin);
+		
+		
+		
+	}
+}
+
+
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
 
 
-	uint32_t numero = 13;
-	uint digitos = 2;
-	uint8_t arreglo[digitos];
-	convertToBcdArray (numero, digitos, arreglo); //se convierte de decimal a BCD
-	for (int i = 0; i < digitos; i++){
-		printf(" valor : %d", arreglo[i]);
-	}
+	uint32_t numero = 735;
+	uint digitos = 3;
+
+	//uint8_t arreglo[digitos];
+	//convertToBcdArray (numero, digitos, arreglo); //se convierte de decimal a BCD
+	//for (int i = 0; i < digitos; i++){
+	//	printf(" valor : %d", arreglo[i]);
+	//}
 	
-	gpioConf_t vectorGPIO[4] = {{GPIO_20,GPIO_OUTPUT},{GPIO_21,GPIO_OUTPUT},{GPIO_22,GPIO_OUTPUT},{GPIO_23,GPIO_OUTPUT}};
+	gpioConf_t vectorGPIOs[4] = {{GPIO_20,GPIO_OUTPUT},{GPIO_21,GPIO_OUTPUT},{GPIO_22,GPIO_OUTPUT},{GPIO_23,GPIO_OUTPUT}};
 	//for ()
-	GPIOInit(vectorGPIO[0].pin, vectorGPIO[0].dir);
-	GPIOInit(vectorGPIO[1].pin, vectorGPIO[1].dir);
-	GPIOInit(vectorGPIO[2].pin, vectorGPIO[2].dir);
-	GPIOInit(vectorGPIO[3].pin, vectorGPIO[3].dir);
+	GPIOInit(vectorGPIOs[0].pin, vectorGPIOs[0].dir);
+	GPIOInit(vectorGPIOs[1].pin, vectorGPIOs[1].dir);
+	GPIOInit(vectorGPIOs[2].pin, vectorGPIOs[2].dir);
+	GPIOInit(vectorGPIOs[3].pin, vectorGPIOs[3].dir);
 
-	//gpioConf_t vectorGPIO_map[3] = {{GPIO_19,GPIO_OUTPUT}, {GPIO_18,GPIO_OUTPUT}, {GPIO_9,GPIO_OUTPUT}};
+	gpioConf_t vectorGPIO_mapeo[3] = {{GPIO_19,GPIO_OUTPUT},{GPIO_18,GPIO_OUTPUT},{GPIO_9,GPIO_OUTPUT}};
+	GPIOInit(vectorGPIO_mapeo[0].pin, vectorGPIO_mapeo[0].dir);
+	GPIOInit(vectorGPIO_mapeo[1].pin, vectorGPIO_mapeo[1].dir);
+	GPIOInit(vectorGPIO_mapeo[2].pin, vectorGPIO_mapeo[2].dir);
 
-	for (int i = 0; i < digitos; i++){
-		//vectorGPIO[i].GPIOInit();
-		modificarStatus(arreglo[i], vectorGPIO);
-	}
+	//for (int i = 0; i < digitos; i++){
+	//
+	//	modificarStatus(arreglo[i], vectorGPIOs);
+	//}
 	
-	//displayLeds(numero, digitos, vectorGPIO, vectorGPIO_map)
+	displayLeds(numero, digitos, vectorGPIOs, vectorGPIO_mapeo);
 	
 }
 /*==================[end of file]============================================*/
